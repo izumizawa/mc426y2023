e2c60c6e2871805e27408d65a2f67d6bbe3740bf
TIwashima <t187462@dac.unicamp.br>
2023-04-26 16:38:47 -0300

    Adiciona requisição de dados de produtos do banco

--- comu/src/main/js/components/ProductList.js
+++ comu/src/main/js/components/ProductList.js
@@ -2,71 +2,38 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import ProductCard from "./ProductCard";
 import Stack from "@mui/material/Stack";
-import Misto from "../assets/images/misto.jpg";
 import Donuts from "../assets/images/donuts.jpg";
-import Fanta from "../assets/images/fanta.jpg";
-
-const mock = [
-  {
-    id: 0,
-    category: "Sobremesas",
-    title: "Donuts de brigadeiro com granulado",
-    description:
-      "Três deliciosas unidades de donuts, recheadas com brigadeiro da melhor" +
-      " qualidade e granulados coloridos – para adoçar sua tarde!",
-    price: "23,00",
-    image: Donuts,
-    image_description: "Donut de brigadeiro com granulado",
-  },
-  {
-    id: 1,
-    category: "Bebidas",
-    title: "Fanta Guaraná – 350ml",
-    description: "Refresque-se com o maravilhoso sabor do verdadeiro guaraná!",
-    price: "6,00",
-    image: Fanta,
-    image_description: "Lata de Fanta Guaraná",
-  },
-  {
-    id: 2,
-    category: "Salgados",
-    title: "Misto Quente",
-    description: "O tradicional pão-com-queijo mais suculento que você já viu.",
-    price: "8,00",
-    image: Misto,
-    image_description: "2 fatias de misto quente",
-  },
-];
+import { useState, useEffect } from "react";
+import { getProductsFromCatalogue } from "../config/firebase";
 
 export default function ProductList(props) {
   const theme = useTheme();
+  const [products, setProducts] = useState({docs: []})
+
+  useEffect(() => {
+    getProductsFromCatalogue("R3o4bdUuF3Til25xtrAn").then(e => {
+      setProducts(e)
+    })
+  }, [])
+
+  const productsList = products.docs.map(function(productDoc) {
+    const product = productDoc.data()
+    return (
+      <ProductCard
+        key={product.title}
+        category={product.category}
+        title={product.title}
+        description={product.description}
+        price={product.price}
+        image={Donuts}
+        image_description={product.image_description}
+      />
+    )
+  })
 
   return (
     <Stack direction="column" spacing={2}>
-      <ProductCard
-        category={mock[2].category}
-        title={mock[2].title}
-        description={mock[2].description}
-        price={mock[2].price}
-        image={mock[2].image}
-        image_description={mock[2].image_description}
-      />
-      <ProductCard
-        category={mock[0].category}
-        title={mock[0].title}
-        description={mock[0].description}
-        price={mock[0].price}
-        image={mock[0].image}
-        image_description={mock[0].image_description}
-      />
-      <ProductCard
-        category={mock[1].category}
-        title={mock[1].title}
-        description={mock[1].description}
-        price={mock[1].price}
-        image={mock[1].image}
-        image_description={mock[1].image_description}
-      />
+      { productsList }
     </Stack>
   );
 }
--- comu/src/main/js/config/firebase.js
+++ comu/src/main/js/config/firebase.js
@@ -1,23 +1,38 @@
-const { initializeApp } = require("firebase/app");
-const { getFirestore, collection, getDocs } = require("firebase/firestore");
+const { initializeApp } = require('firebase/app');
+const { getFirestore, collection, getDocs, where, query, doc, getDoc, documentId } = require('firebase/firestore');
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDefzSC_RK5I201FgCn_Ov2xAiU2fIuDCA",
-  authDomain: "comu-d2ea0.firebaseapp.com",
-  projectId: "comu-d2ea0",
-  storageBucket: "comu-d2ea0.appspot.com",
-  messagingSenderId: "560395082740",
-  appId: "1:560395082740:web:78e695f4bea24d455b37a1",
-};
-
-const app = initializeApp(firebaseConfig);
+    apiKey: "AIzaSyDefzSC_RK5I201FgCn_Ov2xAiU2fIuDCA",
+    authDomain: "comu-d2ea0.firebaseapp.com",
+    projectId: "comu-d2ea0",
+    storageBucket: "comu-d2ea0.appspot.com",
+    messagingSenderId: "560395082740",
+    appId: "1:560395082740:web:78e695f4bea24d455b37a1"
+  };
+  
+  
+const app = initializeApp(firebaseConfig); 
 const db = getFirestore(app);
 
 async function getExamples() {
-  const exampleCol = collection(db, "example");
-  const exampleSnapshot = await getDocs(exampleCol);
-  const exampleList = exampleSnapshot.docs.map((doc) => doc.data());
-  return exampleList;
+    const exampleCol = collection(db, 'example');
+    const exampleSnapshot = await getDocs(exampleCol);
+    const exampleList = exampleSnapshot.docs.map(doc => doc.data());
+    return exampleList;
+  }
+
+async function getProductsFromCatalogue(catalogueId) {
+  const junctionCol = collection(db, 'junction_catalogue_product');
+  const junctionQ = query(junctionCol, where("catalogue", "==", catalogueId));
+  const junctions = await getDocs(junctionQ);
+
+  const productsCol = collection(db, 'product')
+  const productDocs = junctions.docs.map(document => doc(db, `product/${document.data().product}`))
+  const productsQ = query(productsCol, where('__name__', "in", productDocs))
+  const products = await getDocs(productsQ)
+
+  return products
 }
 
-export { getExamples };
+export { getExamples, getProductsFromCatalogue };
