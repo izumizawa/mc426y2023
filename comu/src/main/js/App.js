1ed1630ebac6b90599abf1450e4131e51159a913
JonasCardoso <jonascard60@gmail.com>
2023-04-26 20:20:12 -0300

    feat: adicionar tela de bem-vindo

--- comu/src/main/js/App.js
+++ comu/src/main/js/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import RestaurantSignUp from './screens/restaurantRegister/RestaurantSignUp';
+import Welcome from './screens/welcome/Welcome';
 
 function App() {
   return (
-    <RestaurantSignUp/>
+    <Welcome/>
   );
 }
 
---
+++ comu/src/main/js/screens/welcome/Welcome.jsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <a>
+        comú
+      </a>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function Welcome() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 45,
+              mx: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <StorefrontIcon sx={{ color: 'primary.main' }} />
+            <Typography component="h1" variant="h4" sx={{ mt: 1 }}>
+              Seja bem-vindo ao Comú!
+            </Typography>
+            <Typography component="h1" variant="h6" sx={{ mt: 1 }}>
+              Conheça nossa plataforma e faça parte
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 4 }}>
+            <Grid container spacing={3}>                    
+              <Grid item xs={12}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mb: 2 }}
+              >
+                Prosseguir para o cadastro
+              </Button>
+              </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 8 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+}
