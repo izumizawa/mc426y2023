156597271997c2c001990eb99fe65af29e2b2a1e
joao <joao.andrade@petlove.com.br>
2023-06-25 13:47:39 -0300

    fix: random image to restaurants

--- src/main/js/components/RestaurantItem.js
+++ src/main/js/components/RestaurantItem.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { CardMedia, Card, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 import Donuts from "../assets/images/donuts.jpg";
+import Fanta from "../assets/images/fanta.jpg";
+import Misto from "../assets/images/misto.jpg";
+import Pizza from "../assets/images/pizza.jpg";
+import Sushi from "../assets/images/sushi.jpg";
+import Frites from "../assets/images/frites.jpg";
+
+import styles from "./RestaurantItem.module.css";
+
+const randomImage = {
+    0: Donuts,
+    1: Fanta,
+    2: Misto,
+    3: Pizza,
+    4: Sushi,
+    5: Frites
+}
 
 export default function RestaurantItem({ store }) {
     const navigate = useNavigate();
 
-    const goToStoreDetails = () => navigate(`/restaurantes/${store.id}`)
+    const goToStoreDetails = () => navigate(`/restaurantes/${store.id}`, { state: { store } })
+
+    const chooseRandomImage = useMemo(() => {
+        const randomNumber = Math.floor(Math.random() * 6);
+
+        return randomImage[randomNumber]
+    }, [])
 
     return (
-        <Card onClick={goToStoreDetails} style={{ margin: '1rem', maxWidth: '200px' }} >
+        <Card onClick={goToStoreDetails} className={styles.card} >
             <CardMedia
                 component="img"
-                image={Donuts}
+                image={chooseRandomImage}
+                style={{
+                    aspectRatio: '3/2',
+                }}
             />
             <Box>{store.name}</Box>
         </Card>
--- src/main/js/helpers.js
+++ src/main/js/helpers.js
@@ -1,1 +1,7 @@
 export const validateEmail = (email) => email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+export const formatAddress = (addressObject) => {
+    const { address, addressCity, addressNumber, addressState, zipCode } = addressObject;
+
+    return `${address} ${addressNumber}, ${addressCity} - ${addressState} - ${zipCode}`
+}
